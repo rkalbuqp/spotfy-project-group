f54f59d646bee2de6e511aee9995c2108a273187
Yumi Kato <leilayumi07@gmail.com>
2024-07-20 22:21:25 -0300

    chore(init): movendo imagens para nova pasta assets, correção do webpack.config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles/styles.scss";
+import "./assets/the-lumineers.png";
+import "./assets/back-icon.png";
+import "./assets/ho-hey.png";
+import "./assets/ophelia.png";
+import "./assets/spotless.png";
